455f1698b4045cdce03eb7e768a816d02c6d90a4
Adam Zemmoura <adamzemmoura@gmail.com>
2018-03-02 23:31:44 +0100

    added custom 404 and 500 error pages and routes

--- application.py
+++ application.py
@@ -93,5 +93,13 @@ def signup():
         return redirect(url_for('index'))
     return render_template('signup.html', title='Sign Up', form=form, user=session.get('user'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
