a177a8996cb258aff4b613ce8ca6c20a92a7f20a
Adam Zemmoura <adamzemmoura@gmail.com>
2018-03-02 22:48:05 +0100

    login, logout & registration

--- application.py
+++ application.py
@@ -1,12 +1,22 @@
 import os
 
-from flask import Flask, session, render_template, redirect, url_for
+from flask import Flask, session, render_template, redirect, url_for, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from forms import SignUpForm, LoginForm
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, current_user, login_user, login_required
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+# set up Bootstrap
+bootstrap = Bootstrap(app)
+
+# setup flask-login
+login = LoginManager(app)
+login.login_view = 'login'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -21,26 +31,67 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# forms imports db so import after db setup
+from forms import SignUpForm, LoginForm
+
 # set secret key in app.config
 app.config["SECRET_KEY"] = "you-will-never-guess-this"
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    if session.get('user') is None:
+        return redirect(url_for('login'))
+    return render_template('index.html', user=session.get('user'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    # check to see if the user is logged in already, redirect to index.html if so
+    if session.get('user'):
+        return redirect(url_for('index'))
+
     form = LoginForm()
-    return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        # check to see if the user exists in the database
+        user = db.execute("SELECT * FROM users WHERE email = :email", {'email': form.email.data}).fetchone()
+
+        # if user doesn't exist or p/w wrong, flash a message to user and re-render login page
+        if user is None or not check_password_hash(user.password_hash, form.password.data):
+            flash('Invalid email or password')
+        else:
+            # create a new User object using the user_data
+            session['user'] = user.first_name + ' ' + user.last_name
+            # log user in and redirect to index.html
+            user = session.get('user')
+            flash(f'Successfully logged in as {user}!')
+            return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     # log user out of session
+    session['user'] = None
+
     return redirect(url_for('index'))
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = SignUpForm()
-    return render_template('signup.html', title='Sign Up', form=form)
 
-app.run(debug=True)
+    if session.get('user'):
+        return redirect(url_for('index'))
+
+    form = SignUpForm()
+
+    if form.validate_on_submit():
+        password_hash = generate_password_hash(form.password.data)
+        db.execute("INSERT INTO users (first_name, last_name, email, password_hash) VALUES (:first_name, :last_name, :email, :password_hash)",
+            {"first_name": form.first_name.data, "last_name": form.last_name.data, "email": form.email.data, "password_hash": password_hash})
+        db.commit()
+        flash(f'{form.first_name.data} {form.last_name.data} is now registered')
+        session['user'] = '{form.first_name.data} {form.last_name.data}'
+        return redirect(url_for('index'))
+    return render_template('signup.html', title='Sign Up', form=form, user=session.get('user'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,15 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+from application import db
 
 class SignUpForm(FlaskForm):
     first_name = StringField('First Name :', validators=[DataRequired()])
     last_name = StringField('Last Name :', validators=[DataRequired()])
-    email = StringField('Email : ', validators=[DataRequired()])
-    password = PasswordField('Password : ', validators=[DataRequired()])
+    email = StringField('Email : ', validators=[DataRequired(), Email()])
+    password = PasswordField('Password : ', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm Password : ', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+    def validate_email(self, field):
+        if db.execute("SELECT * FROM users WHERE email = :email", {'email': field.data}).fetchone():
+            raise ValidationError('Email already registered.')
+
 class LoginForm(FlaskForm):
-    email = StringField('Email : ', validators=[DataRequired()])
+    email = StringField('Email : ', validators=[DataRequired(), Email()])
     password = PasswordField('Password : ', validators=[DataRequired()])
     submit = SubmitField('Log In')
